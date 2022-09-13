bc4be7c6e4f6c0798a4e4408f092b8961ebaeb81
Ilya Levshenkov <ivl1969@mail.ru>
2022-09-13 17:34:52 +0300

    Задача 23

---
+++ Ex23/Program.cs
@@ -0,0 +1,33 @@
+﻿;// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string GetTripleTable(int n)
+{
+    string tripleString = "";
+  
+    if(n<1)
+    { 
+        return "неверное число было введено Вами";
+    }
+    else 
+    {
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n)
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}";
+        }
+        else
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}, ";
+        }
+    }
+    return tripleString;
+
+}
+}
+
+Console.Write("Input number: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write($"Таблица кубов от 1 до {x} = {GetTripleTable(x)}");
